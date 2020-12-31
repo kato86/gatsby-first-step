1f376271bff16f33e90a52ad4c191c63bdea8550
Kamil <tomala.kamil@hotmail.com>
2020-12-31 23:14:19 +0100

    First use Graphql

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography';
-import fairyGateTheme from 'typography-theme-fairy-gates';
+import kirkhamTeme from 'typography-theme-kirkham';
 
-const typography = new Typography(fairyGateTheme);
+const typography = new Typography(kirkhamTeme);
 
-export const { scale, rhythm, options } = typography;
 export default typography;
+export const rhythm = typography.rhythm;
